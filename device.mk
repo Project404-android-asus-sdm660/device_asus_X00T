5ec20f016cc1c550797def25f26047f8c45f184e
karthik1896 <karthik1896@outlook.com>
2022-06-26 20:55:45 +0530

    X00T:Adapt to Project 404
    Signed-off-by: karthik1896 <karthik1896@outlook.com>

--- device.mk
+++ device.mk
@@ -318,7 +318,7 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-voltage
+    $(LOCAL_PATH)/overlay-404
 
 # Permissions
 PRODUCT_COPY_FILES += \
